f42f4aec5599133b2c6a1eb4dec3b8622d1c9911
José Isaac <joseisaacnascimento@gmail.com>
2025-07-24 22:00:42 -0300

    feat: Adiciona funcionalidades de pegar os gastos do banco e mostrar numa tabela e função de adicionar um gasto no banco

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
-import { useState } from "react";
+import { db } from "./Firebase/connect";
+import { addDoc, collection, doc, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 function App() {
+  // Estados para armazenar os dados do formulário
   const [data, setData] = useState("");
   const [descricao, setDescricao] = useState("");
   const [valor, setValor] = useState("");
   const [categoria, setCategoria] = useState("");
+
+  // Estado pra armazenar os gastos
+  const [gastos, setGastos] = useState([]);
+
+  // Função para adicionar gasto no banco
+  const addGasto = async () => {
+    if (data === "" || descricao === "" || valor === "" || categoria === "") {
+      toast.error("Preencha todos os campos");
+      return;
+    }
+    try {
+      await addDoc(collection(db, "gastos"), {
+        data,
+        descricao,
+        valor,
+        categoria,
+      });
+      toast.success("Gasto adicionado com sucesso");
+      // Limpa os campos do formulário
+      setData("");
+      setDescricao("");
+      setValor("");
+      setCategoria("");
+      // Atualiza a lista de gastos
+      getGastos();
+    } catch (error) {
+      toast.error("Erro ao adicionar gasto");
+      console.log(error);
+    }
+  };
+  // Função para pegar os gastos no banco
+  const getGastos = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "gastos"));
+      const gastosData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data().data,
+        descricao: doc.data().descricao,
+        valor: doc.data().valor,
+        categoria: doc.data().categoria,
+      }));
+      setGastos(gastosData);
+      console.log(querySnapshot);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect para buscar os dados do banco
+  useEffect(() => {
+    getGastos();
+  }, []);
 
   return (
     <>
@@ -15,7 +71,13 @@ function App() {
 
         {/* Formulário para adicionar gasto */}
         <section>
-          <form className="flex flex-col gap-4 p-4 border rounded-md">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              addGasto();
+            }}
+            className="flex flex-col gap-4 p-4 border rounded-md"
+          >
             <div className="flex flex-col">
               <label className="font-bold" htmlFor="data">
                 Data de Criação
@@ -82,15 +144,26 @@ function App() {
         <section>
           <table className="w-[80vw] m-auto p-4 border rounded-md">
             <thead>
-              <tr>
-                <th>Descrição</th>
-                <th>Valor</th>
-                <th>Ações</th>
+              <tr className="border p-2">
+                <th className="border p-2">Descrição</th>
+                <th className="border p-2">Valor</th>
+                <th className="border p-2">Categoria</th>
               </tr>
             </thead>
-            <tbody>{/* Aqui você renderiza os gastos */}</tbody>
+            <tbody>
+              {gastos.map((gasto) => {
+                return (
+                  <tr key={gasto.id} className="border p-2">
+                    <td className="border p-1">{gasto.descricao}</td>
+                    <td className="border p-1">{gasto.valor}</td>
+                    <td className="border p-1">{gasto.categoria}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         </section>
+        <ToastContainer />
       </div>
     </>
   );
