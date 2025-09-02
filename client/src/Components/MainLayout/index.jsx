661d3649a3d6c263f44c002a15dc72a17c0eb438
José Isaac <joseisaacnascimento@gmail.com>
2025-09-02 20:19:53 -0300

    style: importar icons para o header e criar uma nav para mobile

--- client/src/Components/MainLayout/index.jsx
+++ client/src/Components/MainLayout/index.jsx
@@ -2,36 +2,74 @@
 import { Outlet, Link } from "react-router-dom";
 // Icons
 import { FaPlus, FaListUl, FaChartPie } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+// Hooks
+import { useState } from "react";
 
 const MainLayout = () => {
+  // Estado para controlar se pode exibir o menu
+  const [menu, setMenu] = useState(false);
   return (
     <>
       <div className="flex flex-col min-h-screen">
         {/* Header */}
         <header className="">
+          {/* Nav */}
           <nav className="min-w-screen min-h-[75px] flex items-center justify-around bg-primary text-white">
             <Link to={"/"}>
-              <h1 className="font-bold text-2xl transition-all duration-200 hover:text-warning ">
+              <h1 className="font-bold text-2xl transition-all duration-200 hover:text-warning cursor-pointer ">
                 DevGastos
               </h1>
             </Link>
-            <ul className="flex gap-4">
+            {/* Menu Desktop */}
+            <ul className="hidden sm:flex  gap-4">
               <li>
                 <Link to={"/novoGasto"} title="Cadastrar">
-                  <FaPlus className="transition-all duration-200 hover:text-warning" />
+                  <FaPlus className="transition-all duration-200 hover:text-warning cursor-pointer" />
                 </Link>
               </li>
               <li>
                 <Link to={"/gastos"} title="Listar">
-                  <FaListUl className="transition-all duration-200 hover:text-warning" />
+                  <FaListUl className="transition-all duration-200 hover:text-warning cursor-pointer" />
                 </Link>
               </li>
               <li>
                 <Link to={"/dashboard"} title="Dashboard">
-                  <FaChartPie className="transition-all duration-200 hover:text-warning" />
+                  <FaChartPie className="transition-all duration-200 hover:text-warning cursor-pointer" />
                 </Link>
               </li>
             </ul>
+
+            {/* Botão menu mobile */}
+            <button
+              onClick={() => {
+                setMenu(!menu);
+                console.log(menu);
+              }}
+              className="md:hidden text-3xl cursor-pointer"
+            >
+              <MdMenu />
+            </button>
+            {/* Nav mobile */}
+            <nav className="hidden">
+              <ul className="flex gap-4">
+                <li>
+                  <Link to={"/novoGasto"} title="Cadastrar">
+                    <FaPlus className="transition-all duration-200 hover:text-warning cursor-pointer" />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/gastos"} title="Listar">
+                    <FaListUl className="transition-all duration-200 hover:text-warning cursor-pointer" />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/dashboard"} title="Dashboard">
+                    <FaChartPie className="transition-all duration-200 hover:text-warning cursor-pointer" />
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </nav>
         </header>
         {/* Main */}
@@ -39,7 +77,7 @@ const MainLayout = () => {
           <Outlet />
         </main>
         {/* Footer */}
-        <footer className="p-2 text-center bg-primary text-white">
+        <footer className="min-w-screen p-2 text-center bg-primary text-white">
           <p className="font-bold">Dev-Gastos</p>
         </footer>
       </div>
