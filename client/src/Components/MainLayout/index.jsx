2b6a7a52cf441fe56898aa3eeb572a18a9d8db0f
José Isaac <joseisaacnascimento@gmail.com>
2025-09-02 12:32:00 -0300

    feat: adicionar ícones com links no header do layout padrão - adicionar negrito ao paragráfo do footer

--- client/src/Components/MainLayout/index.jsx
+++ client/src/Components/MainLayout/index.jsx
@@ -1,21 +1,46 @@
-// Importa Outlet para partes filhas do layout
-import { Outlet } from "react-router-dom";
+// Importa o Link e Outlet para partes filhas do layout
+import { Outlet, Link } from "react-router-dom";
+// Icons
+import { FaPlus, FaListUl, FaChartPie } from "react-icons/fa";
 
 const MainLayout = () => {
   return (
     <>
       <div className="flex flex-col min-h-screen">
         {/* Header */}
-        <header className="flex flex-col p-2 text-2xl text-center bg-primary text-white">
-          <h1>Dev-Gastos</h1>
+        <header className="">
+          <nav className="min-w-screen min-h-[75px] flex items-center justify-around bg-primary text-white">
+            <Link to={"/"}>
+              <h1 className="font-bold text-2xl transition-all duration-200 hover:text-warning ">
+                DevGastos
+              </h1>
+            </Link>
+            <ul className="flex gap-4">
+              <li>
+                <Link to={"/novoGasto"} title="Cadastrar">
+                  <FaPlus className="transition-all duration-200 hover:text-warning" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/gastos"} title="Listar">
+                  <FaListUl className="transition-all duration-200 hover:text-warning" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/dashboard"} title="Dashboard">
+                  <FaChartPie className="transition-all duration-200 hover:text-warning" />
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </header>
         {/* Main */}
         <main className="flex-1">
           <Outlet />
         </main>
         {/* Footer */}
-        <footer className="flex flex-col p-2 text-2xl text-center bg-primary text-white">
-          <p>Dev-Gastos</p>
+        <footer className="p-2 text-center bg-primary text-white">
+          <p className="font-bold">Dev-Gastos</p>
         </footer>
       </div>
     </>
