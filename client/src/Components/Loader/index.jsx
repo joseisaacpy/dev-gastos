08ae8b157660ddfc0a8c185e5afb54744cf7beca
José Isaac <joseisaacnascimento@gmail.com>
2025-09-03 21:33:00 -0300

    style: adicionar fixed para cobrir a tela

--- client/src/Components/Loader/index.jsx
+++ client/src/Components/Loader/index.jsx
@@ -1,8 +1,8 @@
 const Loader = () => {
   return (
     <>
-      <div className="overflow-hidden min-h-screen min-w-screen bg-slate-100 flex flex-col items-center justify-center">
-        <div className="border-4 border-b-gray-800 rounded-full border-t-transparent w-10 h-10 animate-spin"></div>
+      <div className="fixed inset-0 overflow-hidden h-screen w-screen bg-slate-100 flex flex-col items-center justify-center">
+        <div className="border-6 border-primary-dark rounded-full border-t-transparent w-15 h-15 animate-spin"></div>
       </div>
     </>
   );
