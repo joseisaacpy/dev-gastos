4c2d61efcb6c8eaf5ad3b6814c00240a84621ca0
José Isaac <joseisaacnascimento@gmail.com>
2025-10-08 13:55:51 -0300

    fix: corrigir encaminhamento de rota

--- client/src/Pages/NotFound/index.jsx
+++ client/src/Pages/NotFound/index.jsx
@@ -1,19 +1,27 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import { FaArrowLeft } from "react-icons/fa";
+
 const NotFound = () => {
   useEffect(() => {
     document.title = "Página não encontrada";
   });
+
   return (
     <>
       <section className="min-w-screen min-h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-primary to-primary-light">
         <div className="flex flex-col items-center justify-center gap-2">
-          <h1 className="text-4xl font-bold text-white">
+          <h1 className="text-4xl font-bold text-white text-center">
             Ops, página não encontrada...
           </h1>
-          <button className="bg-white p-2 rounded-2xl transition-all duration-300 hover:scale-110">
-            <Link to={"/painel"}>Voltar</Link>
+          <button className="">
+            <Link
+              to={"/"}
+              className="flex items-center justify-center bg-white rounded-full p-4 transition-all duration-300 hover:scale-110 hover:bg-primary hover:text-white text-2xl"
+            >
+              <FaArrowLeft />
+            </Link>
           </button>
         </div>
       </section>
