cbc8f1da14dcc0921678ae6b98a97c3469fcc251
José Isaac <joseisaacnascimento@gmail.com>
2025-10-08 13:56:50 -0300

    refacor: adicionar variavel que armazena links do header

--- client/src/Components/MainLayout/index.jsx
+++ client/src/Components/MainLayout/index.jsx
@@ -11,7 +11,33 @@ const MainLayout = () => {
   const [menu, setMenu] = useState(false);
 
   // Links (usa no mobile e desk)
-  // const menuLinks = [{},{},{}]
+  const menuLinks = [
+    {
+      name: "Cadastrar Gasto",
+      link: "/cadastrar-gasto",
+      icon: <FaPlus />,
+    },
+    {
+      name: "Listar Gastos",
+      link: "/listar-gastos",
+      icon: <FaListUl />,
+    },
+    {
+      name: "Cadastrar Receita",
+      link: "/cadastrar-receita",
+      icon: <FaPlus />,
+    },
+    {
+      name: "Listar Receitas",
+      link: "/listar-receitas",
+      icon: <FaListUl />,
+    },
+    {
+      name: "Dashboard",
+      link: "/dashboard",
+      icon: <FaChartPie />,
+    },
+  ];
 
   // Estado para controlar se o dropdown pode aparecer
   const [showDrop, setShowDrop] = useState(false);
@@ -22,6 +48,7 @@ const MainLayout = () => {
         <header className="text-white">
           {/* Nav */}
           <nav className="min-w-screen min-h-[75px] shadow-2xl flex items-center justify-around bg-primary-dark">
+            {/* Logo */}
             <Link to={"/"}>
               <h1 className="font-bold text-2xl transition-all duration-200 hover:text-warning cursor-pointer ">
                 &lt;DevGastos/&gt;
@@ -29,97 +56,23 @@ const MainLayout = () => {
             </Link>
             {/* Menu Desktop */}
             <ul className="hidden sm:flex gap-4">
-              <li className="relative">
-                <button onClick={() => setShowDrop(!showDrop)}>
-                  <FaPlus className="transition-all duration-200 hover:text-warning cursor-pointer" />
-                </button>
-                {/* Dropdown */}
-                <div
-                  className={`absolute right-0 w-30 bg-primary shadow-lg rounded-md  transition-all duration-300 ${
-                    showDrop
-                      ? "flex flex-col opacity-100"
-                      : "opacity-0 pointer-events-none"
-                  }`}
+              {menuLinks.map((link) => (
+                <li
+                  key={link.name}
+                  className="transition-all duration-200 hover:text-warning cursor-pointer"
                 >
-                  <Link
-                    onClick={() => {
-                      setShowDrop(!showDrop);
-                    }}
-                    to={"/novoGasto"}
-                    className="block text-center rounded-md px-4 py-2 transition-all duration-300 hover:bg-gray-100 hover:text-warning"
-                  >
-                    + Gasto
-                  </Link>
-                  <Link
-                    onClick={() => {
-                      setShowDrop(!showDrop);
-                    }}
-                    to={"/novaReceita"}
-                    className="block text-center rounded-md px-4 py-2 transition-all duration-300 hover:bg-gray-100 hover:text-warning"
-                  >
-                    + Receita
-                  </Link>
-                </div>
-              </li>
-              <li>
-                <Link to={"/gastos"} title="Listar">
-                  <FaListUl className="transition-all duration-200 hover:text-warning cursor-pointer" />
-                </Link>
-              </li>
-              <li>
-                <Link to={"/dashboard"} title="Dashboard">
-                  <FaChartPie className="transition-all duration-200 hover:text-warning cursor-pointer" />
-                </Link>
-              </li>
+                  <Link to={link.link}>{link.name}</Link>
+                </li>
+              ))}
             </ul>
-
-            {/* Botão menu mobile */}
+            {/* Botão Mobile para exibir o menu */}
             <button
-              onClick={() => {
-                setMenu(!menu);
-              }}
-              className="md:hidden text-3xl cursor-pointer"
+              onClick={() => setMenu(!menu)}
+              className="sm:hidden transition-all duration-200 hover:text-warning cursor-pointer"
             >
               <MdMenu />
             </button>
           </nav>
-          {/* Menu mobile */}
-          <ul
-            className={`overflow-hidden w-full flex flex-col md:hidden pl-2 gap-4 bg-primary-dark transition-all duration-500 ${
-              menu ? "max-h-[500px] opacity-100" : "max-h-0"
-            }`}
-          >
-            <li className="mb-2">
-              <Link
-                to={"/novoGasto"}
-                title="Cadastrar"
-                className="flex items-center gap-2 "
-              >
-                <FaPlus className="transition-all duration-200 hover:text-warning cursor-pointer" />
-                <span>Link</span>
-              </Link>
-            </li>
-            <li className="mb-2">
-              <Link
-                to={"/gastos"}
-                title="Listar"
-                className="flex items-center gap-2 "
-              >
-                <FaListUl className="transition-all duration-200 hover:text-warning cursor-pointer" />
-                <span>Link</span>
-              </Link>
-            </li>
-            <li className="mb-2">
-              <Link
-                to={"/dashboard"}
-                title="Dashboard"
-                className="flex items-center gap-2 "
-              >
-                <FaChartPie className="transition-all duration-200 hover:text-warning cursor-pointer" />
-                <span>Link</span>
-              </Link>
-            </li>
-          </ul>
         </header>
         {/* Main */}
         <main className="flex-1">
